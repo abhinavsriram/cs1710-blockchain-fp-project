47e63f6bbf80f74016cf4db65eb56d7d9b26efe9
Abhinav Sriram <abhinav_sriram@brown.edu>
2022-05-04 10:35:03 -0400

    visualizer done, but severely underconstrained

--- chainbuilder_vis.js
+++ chainbuilder_vis.js
@@ -1,29 +1,66 @@
-function makeBlockDiv() {
+function makeBlockDiv(fblock) {
     var block = document.createElement("div")
     block.style.width = "150px"
-    block.style.height = "385px"
+    block.style.height = "380px"
     block.style.border = "thin solid black"
     block.style.display = "flex"
     block.style.margin = "5px"
     block.style.padding = "3px"
     block.style['flex-direction'] = "column"
     block.style['font-size'] = "12px"
+    block.style['align-items'] = "center"
+    block.style['justify-content'] = "center"
+    block.innerHTML = "Block " + fblock.toString()[fblock.toString().length - 1]
     return block
 }
 
 function makeHeaderDiv(blockHeader) {
     var header = document.createElement("div")
     header.style.width = "130px"
-    header.style.height = "85px"
+    header.style.height = "110px"
     header.style.border = "thin solid black"
     header.style.display = "flex"
     header.style.margin = "5px"
     header.style.padding = "3px"
-    header.style['flex-direction'] = "row"
+    header.style['flex-direction'] = "column"
     header.style['font-size'] = "8px"
-    headerDiv.append("block created at: " + blockHeader.join(time) + "\n")
-    headerDiv.append("block nonce: " + blockHeader.join(nonce) + "\n")
-    headerDiv.append("prev block hash: " + blockHeader.join(prevBlockHash) + "\n")
+
+    var blockHeaderNameDiv = document.createElement("div")
+    blockHeaderNameDiv.style.width = "auto"
+    blockHeaderNameDiv.style.height = "auto"
+    blockHeaderNameDiv.style.border = "thin solid black"
+    blockHeaderNameDiv.style.display = "flex"
+    blockHeaderNameDiv.style.margin = "5px"
+    blockHeaderNameDiv.style.padding = "3px"
+    blockHeaderNameDiv.style['align-items'] = "center"
+    blockHeaderNameDiv.style['justify-content'] = "center"
+    blockHeaderNameDiv.append("Block Header")
+    header.append(blockHeaderNameDiv)
+
+    var versionDiv = document.createElement("div")
+    versionDiv.append("Version: " + blockHeader.join(version) + "\n")
+    header.append(versionDiv)
+
+    var timeDiv = document.createElement("div")
+    timeDiv.append("Created at: " + blockHeader.join(time) + "\n")
+    header.append(timeDiv)
+
+    var nonceDiv = document.createElement("div")
+    nonceDiv.append("Nonce: " + blockHeader.join(nonce) + "\n")
+    header.append(nonceDiv)
+
+    var blockSizeDiv = document.createElement("div")
+    blockSizeDiv.append("Block Size: " + blockHeader.join(blocksize) + "\n")
+    header.append(blockSizeDiv)
+
+    var prevHashDiv = document.createElement("div")
+    prevHashDiv.append("Prev Block Hash: " + blockHeader.join(prevBlockHash) + "\n")
+    header.append(prevHashDiv)
+
+    var merkleHashDiv = document.createElement("div")
+    merkleHashDiv.append("Merkle Root Hash: " + blockHeader.join(merkleRootHash) + "\n")
+    header.append(merkleHashDiv)
+
     return header
 }
 
@@ -35,10 +72,25 @@ function makeTransactionsDiv(blockTransactions) {
     txs.style.display = "flex"
     txs.style.margin = "5px"
     txs.style.padding = "3px"
-    txs.style['flex-direction'] = "row"
+    txs.style['flex-direction'] = "column"
     txs.style['font-size'] = "8px"
+
+    var txNameDiv = document.createElement("div")
+    txNameDiv.style.width = "auto"
+    txNameDiv.style.height = "auto"
+    txNameDiv.style.border = "thin solid black"
+    txNameDiv.style.display = "flex"
+    txNameDiv.style.margin = "5px"
+    txNameDiv.style.padding = "3px"
+    txNameDiv.style['align-items'] = "center"
+    txNameDiv.style['justify-content'] = "center"
+    txNameDiv.append("Transactions")
+    txs.append(txNameDiv)
+
     for (const tx in blockTransactions.tuples()) {
-        txsDiv.append(blockTransactions.tuples()[tx] + '\n')
+        var txDiv = document.createElement("div")
+        txDiv.append(blockTransactions.tuples()[tx] + '\n')
+        txs.append(txDiv)
     }
     return txs
 }
@@ -51,9 +103,9 @@ function makeBlockHashDiv(blockHash) {
     hash.style.display = "flex"
     hash.style.margin = "5px"
     hash.style.padding = "3px"
-    hash.style['flex-direction'] = "row"
+    hash.style['flex-direction'] = "column"
     hash.style['font-size'] = "8px"
-    hash.append("block hash: " + blockHash + "\n")
+    hash.append("Block Hash: " + blockHash + "\n")
     return hash
 }
 
@@ -64,8 +116,7 @@ function makeBlock(block) {
     const blockVotes = block.join(votes)
     const blockApproved = block.join(approved)
 
-    blockDiv = makeBlockDiv()
-    blockDiv.innerHTML = "<h1> " + "Block " + block.toString()[block.toString().length - 1] + "</h1>"
+    blockDiv = makeBlockDiv(block)
 
     headerDiv = makeHeaderDiv(blockHeader)
     blockDiv.append(headerDiv)
@@ -80,16 +131,15 @@ function makeBlock(block) {
 }
 
 function createBlockChain(state) {
-    container = document.createElement("div")
-    container.style.width = "100%"
-    container.style.height = "400px"
-    container.style.margin = "5px"
-    container.style.padding = "5px"
-    container.style.display = "flex"
-    container.style['flex-direction'] = "column"
+    blockChainContainer = document.createElement("div")
+    blockChainContainer.style.width = "100%"
+    blockChainContainer.style.height = "400px"
+    blockChainContainer.style.margin = "5px"
+    blockChainContainer.style.display = "flex"
+    blockChainContainer.style['flex-direction'] = "column"
 
     blockChainDiv = document.createElement("div")
-    container.append(blockChainDiv)
+    blockChainContainer.append(blockChainDiv)
     blockChainDiv.style.display = "flex"
     blockChainDiv.style['flex-direction'] = "row"
     blockChainDiv.style['flex-wrap'] = "wrap"
@@ -98,26 +148,26 @@ function createBlockChain(state) {
         const block = state.join(blockchain).join(allBlocks).tuples()[ind]
         blockChainDiv.append(makeBlock(block))
     }
-    return container
+    return blockChainContainer
 }
 
 function createStateDiv(state) {
-    outCont = document.createElement("div")
-    outCont.innerHTML = "<h1> " + "Blockchain State at Timestep " + state.toString()[state.toString().length - 1] + "</h1>"
-    outCont.style.display = "flex"
-    outCont.style.display = "flex"
-    outCont.style['flex-direction'] = "column"
-    outCont.style.margin = "20px 5px"
-    outCont.style.border = "medium solid black"
-    outCont.style.padding = "5px"
+    outerDiv = document.createElement("div")
+    outerDiv.innerHTML = "<h1> " + "Blockchain State at Timestep " + state.toString()[state.toString().length - 1] + "</h1>"
+    outerDiv.style.display = "flex"
+    outerDiv.style.display = "flex"
+    outerDiv.style['flex-direction'] = "column"
+    outerDiv.style.margin = "20px 5px"
+    outerDiv.style.border = "medium solid black"
+    outerDiv.style.padding = "5px"
 
-    divCont = document.createElement("div")
-    divCont.style.display = "flex"
-    divCont.style['flex-direction'] = "row"
-    divCont.style['flex-wrap'] = "wrap"
-    divCont.append(createBlockChain(state))
-    outCont.append(divCont)
-    return outCont
+    innerDiv = document.createElement("div")
+    innerDiv.style.display = "flex"
+    innerDiv.style['flex-direction'] = "row"
+    innerDiv.style['flex-wrap'] = "wrap"
+    innerDiv.append(createBlockChain(state))
+    outerDiv.append(innerDiv)
+    return outerDiv
 }
 
 function createStates() {
